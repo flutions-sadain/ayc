3d1819fcfaa771ab368477af22b62d60140077bd
flutions-sadain <164063563+flutions-sadain@users.noreply.github.com>
2024-05-16 19:04:41 +0530

    Fix  bugs in New Home UI

---
+++ src/data/autocompleteData.js
@@ -0,0 +1,99 @@
+const autoCompleteData = {
+    "summary": {
+        "name": "Guru Monish Amartya Jorepalli",
+        "email": "gurumonish2000@gmail.com",
+        "phonenumber": "8886334398",
+        "skills": [
+            "LLM",
+            "Gen AI",
+            "Python",
+            "Django",
+            "Fast API",
+            "Data science",
+            "Deep Learning",
+            "Machine Learning",
+            "MySQL",
+            "Data analysis",
+            "Pandas",
+            "Numpy",
+            "GCP",
+            "Github"
+        ],
+        "previous_technology_details": [
+            "Data analysis with ETL operations",
+            "Data visualization",
+            "Integration the Cloud SQL, Big Query Tables with Python Scripts",
+            "Pyspark",
+            "Hadoop architecture"
+        ],
+        "previous_company_details": [
+            {
+                "Company Name": "Prodapt",
+                "Location": "Chennai,India",
+                "Duration": "06/2022 - Present",
+                "Role": "Senior Software Engineer",
+                "Responsibilities": [
+                    "Gathering, organizing, and cleaning data from diverse sources",
+                    "Preprocessing data to create visualizations that help identify trends and insights",
+                    "Building data pipelines from data lake/data warehouse for subsequent development of data science models",
+                    "Effectively communicating data-driven decisions within an organization",
+                    "Writing complex logics essential for businesses to get their expected outcomes"
+                ]
+            },
+            {
+                "Company Name": "Cognizant",
+                "Location": "Chennai, India",
+                "Duration": "01/2022 - 06/2022",
+                "Role": "Intern as Data Engineer",
+                "Responsibilities": [
+                    "Worked as a different modules in spark and Hadoop architecture"
+                ]
+            },
+            {
+                "Company Name": "Qbec Infosol",
+                "Location": "Chennai, India",
+                "Duration": "12/2019 - 12/2021",
+                "Role": "Intern as a Full Stack Developer",
+                "Responsibilities": [
+                    "Worked in a real-time project which is a bridge between student and University"
+                ]
+            }
+        ],
+        "projects": [
+            {
+                "Project Name": "Shopping website",
+                "Description": "An attractive website which includes all backend functionalities(django) and it provides easy shopping to the user."
+            },
+            {
+                "Project Name": "UNIERO",
+                "Description": "An attractive website which act as bridge between student and University."
+            },
+            {
+                "Project Name": "Automatic Loan approval",
+                "Description": "This machine learning model was trained with big datastes, and it was programmed to act effectively on different criteria of people who seeks loans from banks or other financial institutions, this is pure data science project and python was used as raw programming language"
+            },
+            {
+                "Project Name": "Speech emotion recognition",
+                "Description": "This machine learning model was trained with various actors audio data sets from kaggle website. This model predicts the human emotion."
+            },
+            {
+                "Project Name": "Auto Spam filtering by using machine learning algorithms",
+                "Description": "This machine learning model is trained by analyzing incoming emails and the feature selection is used to obtain words which allows us to distinguish between ham and spam emails."
+            }
+        ],
+        "certifications": [
+            "Advance Certiﬁcate Programme in Data Science from UpGrad",
+            "Professional IBM Data science from Coursera",
+            "IBM - Machine Learning, Data Analysis, python for data science, AI and developement from Coursera",
+            "Deep Learning from udemy",
+            "Associate Cloud Engineer from udemy",
+            "My SQL and Database design from udemy"
+        ],
+        "additional information": [
+            "Presented the research paper on Speech Emotion Recognition at the IEEE Conference.",
+            "Gold Medalist in Saveetha University."
+        ]
+    }
+};
+
+export default autoCompleteData;
