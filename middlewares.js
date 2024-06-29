0f854d8dcf54bf74258d55adedeaf079bf1d79a8
flutions-sadain <164063563+flutions-sadain@users.noreply.github.com>
2024-06-29 18:47:39 +0530

    delete files

--- middlewares.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.method === 'POST' && req.path === '/uploadFile') {
-      res.json({ message: 'Data successfully submitted' });
-    } else {
-      // Call next() to pass the request to the next middleware
-      next();
-    }
-  };
-  
